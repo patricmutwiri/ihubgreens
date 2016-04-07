225b53173826bf711eba1d0b7b6d999fb31da135
Saworieza <saworieza@gmail.com>
2016-04-07 22:40:43 +0300

    fixed the hulk avatar issue.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407174819) do
+ActiveRecord::Schema.define(version: 20160407193308) do
 
   create_table "greens", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,11 +26,11 @@ ActiveRecord::Schema.define(version: 20160407174819) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "username"
   end
 
   add_index "greens", ["email"], name: "index_greens_on_email", unique: true
